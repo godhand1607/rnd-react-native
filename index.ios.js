e72b317ba0e34049ba306be1f4932624509cc073
godhand1607 <jamcando@gmail.com>
2017-03-07 22:34:27 +0800

    Initial commit

---
+++ index.ios.js
@@ -0,0 +1,21 @@
+// index.ios.js - place code here for iOS!!!
+
+// Import libraries to help create a Component
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
+
+// Create a Component
+const App = () => (
+  <View style={{ flex: 1 }}>
+    <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
+);
+
+// Render it to the device
+AppRegistry.registerComponent('rnd_react_native', () => App);
+
+// Expose for unit testing
+export default App;
